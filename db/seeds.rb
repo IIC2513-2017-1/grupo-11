35417cec65659152d8cdaa586475b596d503695c
Niv Oksenberg <noksenberg@uc.cl>
2017-05-16 13:36:35 -0400

    Trying to fix seed in prod. Modifying seed.rb'

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+
 # Clean
 User.destroy_all
 Proyect.destroy_all
