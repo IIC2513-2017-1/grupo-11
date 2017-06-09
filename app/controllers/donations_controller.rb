c30c85c98804f25804652bb16d1959502d3fd59f
Niv Oksenberg <noksenberg@uc.cl>
2017-06-09 19:23:46 -0400

    Seed and Donations

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,8 +1,12 @@
 class DonationsController < ApplicationController
+  before_action :set_proyect, only: [:create]
+
   def create
-    donation = Donation.create(donation_params)
-    UserMailer.donate(donation.proyect.founder, donation.proyect, donation.amount).deliver_now
-    redirect_to :back
+    if @proyect.due_date > Date.today
+      donation = Donation.create(donation_params)
+      UserMailer.donate(donation.proyect.founder, donation.proyect, donation.amount).deliver_now
+      redirect_to :back
+    end
   end
 
   def destroy
@@ -14,5 +18,9 @@ class DonationsController < ApplicationController
 
   def donation_params
     params.require(:donation).permit(:amount).merge(params.permit(:user_id, :proyect_id))
+  end
+
+  def set_proyect
+    @proyect = Proyect.find(params[:proyect_id])
   end
 end
