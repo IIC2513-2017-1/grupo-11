e2a2e072ce2d7ed48d5dc2f192e7bfe5178b574c
Niv Oksenberg <noksenberg@uc.cl>
2017-06-11 13:47:16 -0400

    Menu working and icons

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,43 +1,43 @@
 /**
  * Created by Niv on 09-06-2017.
  */
-$(document).on('turbolinks:load', function () {
 
-    alert("1");
+// OffCanvass
+var offCanvass = function() {
+    $('body').on('click', '.menu-btn', function(e){
+        $('#offcanvass').addClass('awake');
+        e.preventDefault();
+    });
+};
 
-    // OffCanvass
-    var offCanvass = function() {
-        $('body').on('click', '.menu-btn', function(e){
-            $('#offcanvass').addClass('awake');
-            e.preventDefault();
-        });
-    };
-
-    // Click outside of offcanvass
-    var mobileMenuOutsideClick = function() {
-        $(document).click(function (e) {
-            var container = $("#offcanvass, .js-menu-btn");
-            if (!container.is(e.target) && container.has(e.target).length === 0) {
-                if ( $('#offcanvass').hasClass('awake') ) {
-                    $('#offcanvass').removeClass('awake');
-                }
+// Click outside of offcanvass
+var mobileMenuOutsideClick = function() {
+    $(document).click(function (e) {
+        var container = $("#offcanvass, .js-menu-btn");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            if ( $('#offcanvass').hasClass('awake') ) {
+                $('#offcanvass').removeClass('awake');
             }
-        });
-
-        $(window).scroll(function(){
-            if ( $(window).scrollTop() > 500 ) {
-                if ( $('#offcanvass').hasClass('awake') ) {
-                    $('#offcanvass').removeClass('awake');
-                }
-            }
-        });
-    };
-
-
-    $(function(){
-        offCanvass();
-        mobileMenuOutsideClick();
+        }
     });
 
+    $(window).scroll(function(){
+        if ( $(window).scrollTop() > 500 ) {
+            if ( $('#offcanvass').hasClass('awake') ) {
+                $('#offcanvass').removeClass('awake');
+            }
+        }
+    });
+};
 
-}());
+
+var ready = function(){
+    offCanvass();
+    mobileMenuOutsideClick();
+};
+
+$(document).ready(ready);
+$(document).on('turbolinks:load', ready);
+
+
+
